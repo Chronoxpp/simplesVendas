868e0d05a48f13daee136d5a738cdb5ee07c7c91
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2025-06-06 21:18:30 -0400

    feat: adiciona conexao com banco de dados

---
+++ simplesVendas/src/jdbc/Conexao.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author TADS
+ */
+public class Conexao {
+    public static void main(String[] args)
+    {
+        Connection minhaConexao = conectar();
+    }
+    
+    public static Connection conectar()
+    {
+        Connection conexao = null;
+        
+        String driver = "com.mysql.cj.jdbc.Driver";
+        
+        String url = "jdbc:mysql://localhost:3306/bdvendas?useTimezone=true&serverTimezone=UTC";
+        String user = "root";
+        String senha = "root";
+        
+        try
+        {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, senha);
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+            return conexao;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Ops, algo deu errado: " + e.getMessage());
+            return null;
+        }
+    }
+}
